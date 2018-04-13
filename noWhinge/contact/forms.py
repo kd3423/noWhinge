e59000ae9202ef85d0ecd81e08939f352caf77d5
Aakash Deep <aakash15001@iiitd.ac.in>
2018-04-13 14:48:16 +0530

    contact form updated

--- noWhinge/contact/forms.py
+++ noWhinge/contact/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
 class ContactForm(forms.Form):
-	full_name = forms.CharField()
+	name = forms.CharField()
 	email = forms.EmailField()
 	message = forms.CharField(max_length=1500)
+
+	def clean_name(self):
+		return self.cleaned_data.get('name')
+
+	def clean_email(self):
+		return self.cleaned_data.get('email')
+	
+	def clean_message(self):
+		return self.cleaned_data.get('message')
+	
